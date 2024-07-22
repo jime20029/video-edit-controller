3312798a6aaaeca59a7681d351de66be7664d7a3
Hajime Umeda <jimbasuke@yahoo.co.jp>
2024-07-22 20:26:01 +0900

    original_video等の保持をtupleに変更

--- features/sign.py
+++ features/sign.py
@@ -4,8 +4,10 @@ from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
 from Crypto.Hash import SHA256
 
-original_video = {'a', 'b', 'c', 'd', 'e'}
-edited_video = {'a', 'b'}
+original_video = ('a', 'b', 'c', 'd', 'e')
+edited_video = ('a', 'b', 'e')
+scores_per_segment = {('a', 'b'): 0.2, ('a', 'b', 'e'): 0.6, ('a', 'e'): 0.9}
+threshold = 0.7
 
 # 鍵ペアの生成
 def generate_key_pair():
@@ -18,10 +20,8 @@ def generate_key_pair():
 def sign_original_video(private_key, original_video, allowed_deletion_list):
     key = RSA.import_key(private_key)
 
-    ordered_list = sorted(list(original_video))
-
     # リストを文字列に変換
-    data_str = ''.join(ordered_list)
+    data_str = ''.join(original_video)
 
     # 文字列をバイト列に変換
     original_video_bytes = data_str.encode('utf-8')
@@ -39,18 +39,17 @@ def sign_original_video(private_key, original_video, allowed_deletion_list):
 # 関数f
 def verify_edited_video(edited_video, allowed_list):
     # 許可された削除リストに含まれる編集か検証する処理
-    edited_video_frozenset = frozenset(edited_video)
-    return edited_video_frozenset in allowed_list
+    return edited_video in allowed_list
 
-# 東北大学側システムに問い合わせて塊ごとのスコア取得
+# Tシステムに問い合わせて塊ごとのスコア取得
 def get_segment_scores(original_video):
-    # 東北大学側システムに問い合わせする処理を追加
-    return {frozenset({'a', 'b'}): 0.2, frozenset({'a', 'b', 'e'}): 0.6, frozenset({'a', 'e'}): 0.9}
+    # Tシステムに問い合わせする処理を追加
+    return scores_per_segment
 
-# 東北大学側システムの結果をもとに閾値を決定
+# Tシステムの結果をもとに閾値を決定
 def decide_threshold(segment_and_scores):
     # セグメントと組み合わせとスコアのセットから閾値を決定する処理を追加
-    return 0.5
+    return threshold
 
 # 全ての長さの組み合わせを生成する関数
 def all_combinations(s):
@@ -58,7 +57,7 @@ def all_combinations(s):
 
 # 閾値とスコアから許可リストを作成
 def create_allowed_deletion_list(original_video):
-    ### 東北大学側のシステムに問い合わせ＝＞削除許可リスト作成
+    ### Tのシステムに問い合わせ＝＞削除許可リスト作成
     ### セグメントと閾値から削除許可リストを作成する処理
     segment_and_scores = get_segment_scores(original_video)
     threshold = decide_threshold(segment_and_scores)
@@ -67,7 +66,7 @@ def create_allowed_deletion_list(original_video):
 
 # 動画作成者のセットアップ
 def setup(original_video):
-    ### 東北大学側のシステムに問い合わせ＝＞削除許可リスト作成
+    ### Tのシステムに問い合わせ＝＞削除許可リスト作成
     allowed_deletion_list = create_allowed_deletion_list(original_video)
     #####################
 
@@ -80,6 +79,7 @@ def setup(original_video):
 # 署名を検証
 def verify_signature(original_video, edited_video, allowed_deletion_list, signature, public_key):
 
+    # 編集が適切かどうかを判定
     is_proper_edited_video = verify_edited_video(edited_video, allowed_deletion_list)
 
     key = RSA.import_key(public_key)
@@ -102,6 +102,7 @@ def verify_signature(original_video, edited_video, allowed_deletion_list, signat
 
     verifier = PKCS1_v1_5.new(key)
 
+    # 署名が正当かつ編集が適切ならTrueを返す
     if verifier.verify(h, signature) and is_proper_edited_video:
         return True
     else:
@@ -113,10 +114,8 @@ def main():
 
     public_key, signature, allowed_deletion_list = setup(original_video)
 
-    print('Setup Completed. Start Distributing.')
-
+    print('allowed_deletion_list =', allowed_deletion_list)
     print('edited_video =', edited_video)
-    print('Edited')
 
     verify_result = verify_signature(original_video, edited_video, allowed_deletion_list, signature, public_key)
     
